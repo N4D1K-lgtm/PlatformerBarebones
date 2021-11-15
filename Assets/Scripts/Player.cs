f72388d0cf310ba44c43c21781afe6ae7b7df40f
N4D1K-lgtm <nelsonkidan@gmail.com>
2021-11-14 20:28:48 -0500

    Fixed Horizontal Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,49 +4,42 @@ using UnityEngine;
 
 // Require Controller2D script on gameobject
 [RequireComponent (typeof (Controller2D))]
-public class Player : MonoBehaviour
-{   
+public class Player : MonoBehaviour {   
     // Final velocity(really this is the calculated movement not velocity) passed to Move() function in the character controller
     Vector3 velocity;
 
     // The velocity calculated w/ timestep and gravity
-    float _velocity;
-
+    float _velocityY;
     // The previous frame's velocity
-    float oldVelocity;
+    float oldVelocityY;
+
+    // Handled by SmoothDampFunction
+    float velocityXSmoothing;
 
     Controller2D controller;
 
-    // Handled by smooth damp funtion
-    float velocityXSmoothing;
+    // Horizontal acceleration time
+    public float accelerationTimeAirborne = .2f;
+    public float accelerationTimeGrounded = .1f;
+    
+    // Horizontal movement variables
+    public float maxHorizontalSpeed = 3f;
+    public float horizontalSpeed = .04f;
 
-    // Horizontal acceleration time, lower is faster.
-    float accelerationTimeAirborne = .2f;
-    float accelerationTimeGrounded = .1f;
-
-    // Move speed, total height of player jump and total time to jump
-    public float moveSpeed = 6;
     public float jumpHeight = 4;
     public float timeToJumpApex = .4f;
     
-    public float maxVerticalVelocity = .1f;
-    public float maxHorizontalVelocity = 20f;
-    float verticalVelocity;
+    public float maxVerticalVelocity = 1f;
 
-    // Calculated from jump apex, height and movespeed
+    // Calculated from jump apex, height and horizontal movespeed
     private float gravity;
+    // To Do: Change jumpforce to depend on horizontal movement and jump height;
     private float jumpForce => 2 * jumpHeight / timeToJumpApex;
-
-    bool grounded;
     
-    
-
     // Start is called before the first frame update
     void Start() {
-        
-        // Access 2DController script as controller
+        // Access 2DController class as controller
         controller = GetComponent<Controller2D> ();
-
     }
 
     // Update is called once per frame
@@ -55,43 +48,33 @@ public class Player : MonoBehaviour
         // Initialize input vector for player movement
         Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        // Target horizontal velocity for Smooth Damping
-        float targetVelocityX = input.x * moveSpeed;
-        
-        // SmoothDamp(current pos, target pos, current velocity, smooth time, maxSpeed, deltaTime)
-        velocity.x = targetVelocityX * Time.deltaTime;
-        //Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne, maxHorizontalVelocity, Time.deltaTime);
-        
+        float targetVelocityX = input.x * horizontalSpeed;
 
         // If space is pressed and player is touching ground apply jump velocity
         if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below) {
-            _velocity = jumpForce;
+            _velocityY = jumpForce;
         }
         
         // Save last calculated _velocity to oldVelocity
-        oldVelocity = _velocity;
-        // Calculate new _velocity from gravity and timestep
-        _velocity += gravity * Time.deltaTime;
-        // Apply Average oldVelocity and new _velocity * timestep
-        velocity.y = (oldVelocity + _velocity) * .5f * Time.deltaTime; 
+        oldVelocityY = _velocityY;
+        // Calculate new _velocityY from gravity and timestep
+        _velocityY += gravity * Time.deltaTime;
         
-        // An attempt to limit vertical velocity but doesnt seem to work
-        Mathf.Clamp(velocity.y, -maxVerticalVelocity, maxVerticalVelocity);
-        
+        // Apply average oldVelocityY and new _velocityY * timestep
+        velocity.y = (oldVelocityY + _velocityY) * .5f * Time.deltaTime;
+
+        // Clamp vertical velocity in between +/- of maxVerticalVelocity;
+        velocity.y = Mathf.Clamp(velocity.y, -maxVerticalVelocity, maxVerticalVelocity);
+
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne, maxHorizontalSpeed, Time.deltaTime);
         // Call move function in 2DController
         controller.Move(velocity);
-
-        // Print Velocity
-        Debug.Log(velocity);
-        
-        // Print if player is on slope
-        Debug.Log(controller.collisions.climbingSlope);
 
         //If 2DController detects a collision above or below player
         if (controller.collisions.below) {
             // Stop the player from moving
             velocity.y = 0;
-            // Prevent gravity from being applied to _velocity
+            // Prevent gravity from being applied to _velocityY
             gravity = 0;
         } else {
 
@@ -99,11 +82,11 @@ public class Player : MonoBehaviour
             gravity = -2 * jumpHeight / (timeToJumpApex * timeToJumpApex);
         }
 
-        if(controller.collisions.above) {
+        if (controller.collisions.above) {
             // If player collides with ceiling, set velocity.y to 0 to stop player movement
             velocity.y = 0;
             // Prevent the previous frames velocity being applied to the player (this is to prevent hang time on the ceiling if a jump is interrupted)
-            _velocity = 0;
+            _velocityY = 0;
         }
     }
 }
